fae546527d66771e7cd31b8f92662ab32a785793
Raed Fadhlaoui <fadhlaouiraied@hotmail.com>
2021-06-01 22:28:45 +0100

    clean

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,11 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
-import { Table, Space } from "antd"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import "./index.css"
 
-const query = graphql`
+export const query = graphql`
   query {
     allStrapiRestaurant {
       edges {
@@ -13,6 +13,9 @@ const query = graphql`
           strapiId
           name
           description
+          image {
+            url
+          }
         }
       }
     }
@@ -27,32 +30,57 @@ const query = graphql`
   }
 `
 
-const IndexPage = () => (
-  <Layout>
-    <Seo title="Home" />
-    <StaticQuery
-      query={query}
-      render={data => (
-        <div>
-          <h3>Restaurant</h3>
-          <ul>
-            {data.allStrapiRestaurant.edges.map(restaurant => (
-              <li key={restaurant.node.strapiId}>
-                {restaurant.node.name}
-                <ul>{restaurant.node.description}</ul>
-              </li>
-            ))}
-          </ul>
-          <h3>Categories</h3>
-          <ul>
-            {data.allStrapiCategory.edges.map(category => (
-              <li key={category.node.strapiId}>{category.node.name}</li>
-            ))}
-          </ul>
-        </div>
-      )}
-    />
-  </Layout>
-)
-
+function IndexPage({ data }) {
+  // const image = getImage(data.allStrapiRestaurant.edges.node.image)
+  return (
+    <Layout>
+      <Seo title="Home" />
+      {/* Method One */}
+      <StaticQuery
+        query={query}
+        render={data => (
+          <div>
+            <h3>Restaurant</h3>
+            <ul>
+              {data.allStrapiRestaurant.edges.map(restaurant => (
+                <li key={restaurant.node.strapiId}>
+                  {restaurant.node.name}
+                  <ul>{restaurant.node.description}</ul>
+                  <GatsbyImage
+                    image={getImage(restaurant.node.image)}
+                    alt={restaurant.node.name}
+                  />
+                </li>
+              ))}
+            </ul>
+            <h3>Categories</h3>
+            <ul>
+              {data.allStrapiCategory.edges.map(category => (
+                <li key={category.node.strapiId}>{category.node.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      />
+      {/* Method Two */}
+      {/* <section>
+        <h3>Restaurant</h3>
+        <ul>
+          {data.allStrapiRestaurant.edges.map(restaurant => (
+            <li key={restaurant.node.strapiId}>
+              {restaurant.node.name}
+              <ul>{restaurant.node.description}</ul>
+            </li>
+          ))}
+        </ul>
+        <h3>Categories</h3>
+        <ul>
+          {data.allStrapiCategory.edges.map(category => (
+            <li key={category.node.strapiId}>{category.node.name}</li>
+          ))}
+        </ul>
+      </section> */}
+    </Layout>
+  )
+}
 export default IndexPage
